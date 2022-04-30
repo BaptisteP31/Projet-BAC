30d04aa5dbb014800cbb58732bf905ae62fd7bcf
Baptiste P <paqueriaudbaba@gmail.com>
2022-04-30 21:16:24 +0200

    Added GPL headers

--- code/main/main.ino
+++ code/main/main.ino
@@ -1,10 +1,27 @@
+/* 
+ * This file is part of the Projet-BAC (https://github.com/BaptisteP31/Projet-BAC).
+ * Copyright (c) 2022 Paqueriaud Baptiste.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "hbridge.h"
 #include "led_indicator.h"
 #include "buzzer_indicator.h"
 
 //On crée un tableau contenant l'ensemble des pins relatifs aux moteurs
 const int E1=3, M1=14, E2=10, M2=15, E3=11, M3=16, E4=9, M4=17; 
-const int pins[8] = {E1, M1, E2, M2, E3, M3, E4, M4};
+int pins[8] = {E1, M1, E2, M2, E3, M3, E4, M4};
 
 //Objets ledR, ledG de la classe ledindicator, delai de 200ms
 ledindicator ledR(20, 200); ledindicator ledG(21, 200);
