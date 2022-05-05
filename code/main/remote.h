5aead6106de0b940931ab0ecbe6bc92e7576bc49
Baptiste P <paqueriaudbaba@gmail.com>
2022-05-05 23:20:33 +0200

    changed GPL header

--- code/main/remote.h
+++ code/main/remote.h
@@ -12,7 +12,25 @@
    This source code is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.    
+   version 2.1 of the License, or (at your option) any later version.
+
++-----------------------------------------------------------------------------------+
+| * This file is part of the Projet-BAC (https://github.com/BaptisteP31/Projet-BAC).|
+| * Copyright (c) 2022 Paqueriaud Baptiste.                                         |
+| *                                                                                 |
+| * This program is free software: you can redistribute it and/or modify            |
+| * it under the terms of the GNU General Public License as published by            |
+| * the Free Software Foundation, version 3.                                        |
+| *                                                                                 |
+| * This program is distributed in the hope that it will be useful, but             |
+| * WITHOUT ANY WARRANTY; without even the implied warranty of                      |
+| * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU                |
+| * General Public License for more details.                                        |
+| *                                                                                 |
+| * You should have received a copy of the GNU General Public License               |
+| * along with this program. If not, see <http://www.gnu.org/licenses/>.            |
++-----------------------------------------------------------------------------------+
+                                                                                     
 */
 
 //////////////////////////////////////////////
